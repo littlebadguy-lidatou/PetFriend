a4b2cc8f9c52e663c8988c25c0f20ea5e9051178
littlebadguy-lidatou <76060600+littlebadguy-lidatou@users.noreply.github.com>
2020-12-16 01:22:41 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# FunSchool
+微信小程序-你是刁难我胖虎
+![image](screenshot/01.jpg)
+![image](screenshot/02.jpg)
+![image](screenshot/03.jpg)
---
+++ app.js
@@ -0,0 +1,8 @@
+//app.js
+App({
+    globalData: {
+        showapi_appid: '41809',
+        showapi_sign: '5db8bfb78aff48309b85032d8e2b3db1',
+        maxResult: 20,
+    }
+})
